acaa5a2263ea0e8b9643ceafde7dcddc0919422f
RICARDO <RICARDO@192.168.4.93>
2016-05-11 17:25:42 -0500

    Interfaz de registro cambiada con tabla.

--- app/declaraciones/decs-lstorage.service.js
+++ app/declaraciones/decs-lstorage.service.js
@@ -1,23 +1,25 @@
 //Almacena usando el servicio $localStorage de ngStorage.
-(function() {
+(function () {
     'use strict';
 
     angular
-        .module('gasolinaApp')
-        .service('DeclaracionesService', DeclaracionesService);
+            .module('gasolinaApp')
+            .service('DeclaracionesService', AsignacionesService);
 
-    DeclaracionesService.$inject = ['$localStorage'];
-    function DeclaracionesService($localStorage) {
-        var ls = $localStorage.$default({ declaraciones: [] });
+    AsignacionesService.$inject = ['$localStorage'];
+    function AsignacionesService($localStorage) {
+        var ls = $localStorage.$default({asignaciones: []});
         this.all = all;
         this.save = save;
 
-        function all() { return ls.declaraciones; }
+        function all() {
+            return ls.asignaciones;
+        }
 
-        function save(declaracion) {
+        function save(asignacion) {
             //Retorna true o false, si guardó o no, respectivamente.
-            var cantidadAnterior = ls.declaraciones.length;
-            var cantidadNueva = ls.declaraciones.push(declaracion);
+            var cantidadAnterior = ls.asignaciones.length;
+            var cantidadNueva = ls.asignaciones.push(asignacion);
             return cantidadAnterior < cantidadNueva;
         }
     }
--- app/declaraciones/registrar.controller.js
+++ app/declaraciones/registrar.controller.js
@@ -1,45 +1,24 @@
-(function() {
+(function () {
     'use strict';
 
     angular
-        .module('gasolinaApp')
-        .controller('RegistrarController', RegistrarController);
+            .module('gasolinaApp')
+            .controller('RegistrarController', RegistrarController);
 
-    RegistrarController.$inject = ['DeclaracionesService', 'MesesService', 'DepartamentosService', 'AniosService'];
-    function RegistrarController(DeclaracionesService, MesesService, DepartamentosService, AniosService) {
+    RegistrarController.$inject = ['DeclaracionesService', 'HorasService', 'SalasService'];
+    function RegistrarController(DeclaracionesService, HorasService, SalasService) {
         console.log("Entró a RegistrarController");
         var vm = this;
 
         //Declaraciones de variables en orden alfabético.
-        vm.anios = AniosService.getAnios();
+        vm.salas = SalasService.getSalas();
         vm.calcular = calcular;
-        vm.cargarMunicipios = cargarMunicipios;
-        vm.departamentos = DepartamentosService.getDepartamentos();
+        vm.horas = HorasService.getHoras();
         vm.guardar = guardar;
-        vm.meses = MesesService.getMeses();
 
         //Funciones, en orden alfabético
         function activate() {
-            vm.declaracion = {};
-            vm.declaracion.municipio = "";
-            vm.municipios = [];
-            //Así son las clases en Javascript ES 5.
-            var Gasolina = function(clase) {
-                this.clase = clase;
-                this.galones_gravado = 0;
-                this.precio_referencia = 0;
-                this.porcentaje_alcohol = 0;
-                this.base_gravable = 0;
-                this.sobretasa = 0;
-            };
-            vm.declaracion.gasolinas_attributes = [
-                new Gasolina("Gasolina corriente básica"),
-                new Gasolina("Gasolina corriente oxigenada"),
-                new Gasolina("Gasolina extra básica"),
-                new Gasolina("Gasolina extra oxigenada"),
-                new Gasolina("Gasolina importada"),
-                new Gasolina("Gasolina NAL. ZON. ESP. FRONT")
-            ];
+            vm.asignacion = {};
         }
 
         function calcular(gasolina) {
@@ -47,36 +26,31 @@
             gasolina.sobretasa = gasolina.base_gravable * 0.06;
         }
 
-        function cargarMunicipios() {
-            vm.declaracion.municipio = "";
-            vm.municipios = DepartamentosService.getMunicipios(vm.declaracion.departamento);
-        }
-
         function guardar() {
-            vm.declaracion.correccion_fecha = $('#datepicker').val();
+            vm.declaracion.fecha = $('#datepicker').val();
             console.log(vm.declaracion);
-            /*lstorage o memory
+            //lstorage o memory
             if (DeclaracionesService.save(vm.declaracion)) {
                 activate();
                 vm.respuestaRegistro = "Registró correctamente";
             } else {
                 vm.respuestaRegistro = "Error registrando.";
-            }*/
-            DeclaracionesService.save(vm.declaracion)
-                .then(function(response) {
-                    console.log(response);
-                    activate();
-                    vm.respuestaRegistro = "Registró correctamente";
-                })
-                .catch(function(error) {
-                    console.log(error);
-                    vm.respuestaRegistro = error.statusText;
-                })
-                .finally(function() { 
-                    $("#modalRegistro").modal(); 
-                });
+            }
+            $("#modalRegistro").modal();
+//            DeclaracionesService.save(vm.declaracion)
+//                    .then(function (response) {
+//                        console.log(response);
+//                        activate();
+//                        vm.respuestaRegistro = "Registró correctamente";
+//                    })
+//                    .catch(function (error) {
+//                        console.log(error);
+//                        vm.respuestaRegistro = error.statusText;
+//                    })
+//                    .finally(function () {
+//                        $("#modalRegistro").modal();
+//                    });
         }
-
         activate();
     }
 })();
